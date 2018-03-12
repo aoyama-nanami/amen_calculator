d4232088ca4cce8bde4848e27176967ec4ecc49b
Aoyama Nanami <aoyama_nanami@sakurasou>
2018-03-12 21:23:11 +0800

    Add data for square+extra attack

--- raw_data/gen.py
+++ raw_data/gen.py
@@ -3,18 +3,24 @@ import os
 import os.path
 
 PATTERN = re.compile(r'(\d+) (\d+) (\d+) http://.*layout=(\d+).*')
+
+inputs = [
+    ('./square', 'SQUARE_{0}'),
+    ('./square_extra_attack', 'SQUARE_EXTRA_ATTACK_{0}')]
+
 with open('data.js', 'w') as output:
-  for f in os.listdir('./square'):
-    with open(os.path.join('../raw_data/square', f)) as source:
-      var_name = 'SQUARE_{0}'.format(f[0])
-      print >> output, 'export const {0} = {{}};'.format(var_name)
-      for line in source:
-        m = PATTERN.match(line)
-        if m:
-          g = m.groups()
-          print >> output, '{0}[{1}] = {0}[{1}] || {{}};'.format(
-              var_name, *m.groups())
-          print >> output, '{0}[{1}][{2}] = \'{4}\';'.format(
-              var_name, *m.groups())
-      print >> output, ''
+  for path, var_name_template in inputs:
+    for f in os.listdir(path):
+      with open(os.path.join(path, f)) as source:
+        var_name = var_name_template.format(f[0])
+        print >> output, 'export const {0} = {{}};'.format(var_name)
+        for line in source:
+          m = PATTERN.match(line)
+          if m:
+            g = m.groups()
+            print >> output, '{0}[{1}] = {0}[{1}] || {{}};'.format(
+                var_name, *m.groups())
+            print >> output, '{0}[{1}][{2}] = \'{4}\';'.format(
+                var_name, *m.groups())
+        print >> output, ''
 
